cba220647b2a4c6a3d550f4c3d3c3d838473d7c2
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2024-03-03 14:36:21 -0800

    Face rec APIs

--- src/main/java/com/sjsu/proxyAuth/Service/LocationService.java
+++ src/main/java/com/sjsu/proxyAuth/Service/LocationService.java
@@ -4,8 +4,6 @@ import com.sjsu.proxyAuth.Repository.LocationRepo;
 import com.sjsu.proxyAuth.model.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 import java.util.List;
 
 @Service
